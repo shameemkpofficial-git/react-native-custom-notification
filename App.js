4787cec955a585e89454c723835c63bf9071cc3c
Shameem <89739674+shameemkpofficial-git@users.noreply.github.com>
2022-12-07 18:46:31 +0530

    commit message

---
+++ App.js
@@ -0,0 +1,19 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+});
